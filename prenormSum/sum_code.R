1506904763af1a3896900915f3e70d4695e2b989
Aaron Lun <alun@wehi.edu.au>
2015-11-26 15:15:07 +0000

    Added test code and justification.

---
+++ prenormSum/sum_code.R
@@ -0,0 +1,62 @@
+# This function sorts cells by their library sizes, and generates an ordering vector.
+
+.generateSphere <- function(lib.sizes) {
+    nlibs <- length(lib.sizes)
+    o <- order(lib.sizes)
+    even <- seq(2,nlibs,2)
+    odd <- seq(1,nlibs,2)
+    out <- c(o[odd], rev(o[even]))
+    c(out, out)
+}
+
+# This contains the function that performs normalization on the summed counts.
+
+normalizeBySums <- function(counts, sizes=c(20, 40, 60, 80, 100)) {
+    ncells <- ncol(counts)
+    if (any(sizes >= ncells)) { 
+        stop("not enough cells for specified 'sizes'") 
+    }
+
+    # Computing the necessary statistics.
+    lib.sizes <- colSums(counts)
+    sphere <- .generateSphere(lib.sizes)
+    exprs <- t(t(counts)/lib.sizes)
+    ave.cell <- rowMeans(exprs)
+
+    # Using our summation approach.
+    design <- list()
+    output <- list()
+
+    for (size in sizes) {
+        all.mat <- matrix(0L, ncells, ncells)
+        out.nf <- numeric(ncells)
+
+        for (it in seq_len(ncells)){
+            chosen <- sphere[it:(it+size-1L)] 
+            all.mat[it,chosen] <- 1L
+            cur.combined <- rowSums(exprs[,chosen])
+            out.nf[it] <- median(cur.combined/ave.cell)
+        }
+
+        design <- c(design, list(all.mat))
+        output <- c(output, list(out.nf))
+    }
+
+    design <- do.call(rbind, design)
+    output <- unlist(output)
+
+    # Adding extra equations to guarantee solvability (downweighted).
+    weights <- rep(c(1, 0.00001), c(nrow(design), ncells))
+    design <- rbind(design, diag(ncells))
+    output <- c(output, apply(exprs/ave.cell, 2, median))
+    root.weights <- sqrt(weights)
+    final.nf <- solve(qr(design * root.weights), output * root.weights)
+
+    # Returning size factors, rather than normalization factors.
+    final.sf <- final.nf * lib.sizes
+    final.sf <- final.sf/mean(final.sf[final.sf>0])
+    return(final.sf)
+}
+
+
+
---
+++ prenormSum/test.R
@@ -0,0 +1,61 @@
+# Defining functions.
+
+source("sum_code.R")
+
+fitNorm <- function(nf, truth) {
+    resids <- log2(nf) - log2(truth)
+    lm(resids ~ 1, na.action=na.exclude)
+}
+
+# Generating some really noisy count data.
+
+set.seed(100)
+ngenes <- 10000
+nlibs <- 200
+true.facs <- 2^rnorm(nlibs, sd=0.5)
+noisy.counts <- matrix(rnbinom(ngenes*nlibs, mu=10*true.facs, size=0.1), 
+                       ncol=nlibs, byrow=TRUE)
+
+# TMM with raw counts:
+require(edgeR)
+tmm.sf <- calcNormFactors(noisy.counts) * colSums(noisy.counts)
+tmm.fitted <- fitNorm(tmm.sf, true.facs)
+
+# TMM with averaged counts:
+combined <- cbind(rowSums(noisy.counts), noisy.counts)
+tmm2.sf <- calcNormFactors(combined) * colSums(combined)
+tmm2.sf <- tmm2.sf[-1]
+tmm2.fitted <- fitNorm(tmm2.sf, true.facs)
+
+# Size factors with raw counts (must remove zeros for both geometric mean and for each library):
+logvals <- log(noisy.counts)
+logvals[is.infinite(logvals)] <- 0
+gm <- exp(rowMeans(logvals))
+size.sf <- apply(noisy.counts, 2, function(u) median((u/gm)[u > 0]))
+size.fitted <- fitNorm(size.sf, true.facs)
+
+# Size factors with averaged counts (must remove zeros in each library):
+averaged <- rowMeans(noisy.counts)
+size2.sf <- apply(noisy.counts, 2, function(u) median((u/averaged)[u > 0]))
+size2.fitted <- fitNorm(size2.sf, true.facs)
+
+# Size factors with summation:
+final.sf <- normalizeBySums(noisy.counts)
+final.fitted <- fitNorm(final.sf, true.facs)
+
+pdf("benefit.pdf")
+true.log.facs <- log2(true.facs)
+all.range <- range(true.log.facs) + c(-0.1, 0.1)
+plot(0, 0, type="n", xlim=all.range, ylim=all.range, ylab="Observed", xlab="Truth")
+
+points(true.log.facs, true.log.facs + residuals(tmm.fitted), pch=16, col="grey")
+points(true.log.facs, true.log.facs + residuals(tmm2.fitted), pch=16, col="dodgerblue")
+points(true.log.facs, true.log.facs + residuals(size.fitted), pch=16, col="orangered")
+points(true.log.facs, true.log.facs + residuals(size2.fitted), pch=16, col="darkred")
+points(true.log.facs, true.log.facs + residuals(final.fitted), pch=16, col="black")
+
+abline(0, 1, col="red", lwd=2)
+legend("bottomright", legend=c("TMM", "TMM vs average", "Size factor (GM)", "Size factor (AM)", "Deconvolved"),
+       pch=16, col=c("grey", "dodgerblue", "orangered", "darkred", "black"))
+dev.off()
+
