a62430ee1038aed5b49fd490b909e2ddde53b1e0
Karsten Bach <karsten_bach@hotmail.com>
2015-12-03 17:51:53 +0000

    Mainly changed plots to fit in the manuscript. Additionally, the Klein analysis now uses our clustering. DE analysis for all four methods included.

--- realdata/Klein.R
+++ realdata/Klein.R
@@ -1,8 +1,5 @@
 # Normalizing Drop-Seq data with Summation strategy
 #
-
-
-
 # ---- Packages ----
 pkgs <- "/nfs/research2/marioni/Karsten/utils/Rpack/"
 library('rJava',lib.loc=pkgs)
@@ -11,6 +8,7 @@ library('xlsx',lib.loc=pkgs)
 library('zoo',lib.loc=pkgs)
 library('DESeq2')
 library('plyr')
+library('edgeR')
 library('dynamicTreeCut',lib.loc=pkgs)
 library('lattice',lib.loc=pkgs)
 library('ggplot2',lib.loc=pkgs)
@@ -22,22 +20,17 @@ source("./functions.R")
 
 # ---- Data ----
 esd0 <- read.csv("../data/GSM1599494_ES_d0_main.csv.bz2", header = FALSE, stringsAsFactors= FALSE)  
-esd2 <- read.csv("../data/GSM1599497_ES_d2_LIFminus.csv.bz2", header = FALSE, stringsAsFactors= FALSE)  
-esd4 <- read.csv("../data/GSM1599498_ES_d4_LIFminus.csv.bz2", header = FALSE, stringsAsFactors= FALSE)  
 esd7 <- read.csv("../data/GSM1599499_ES_d7_LIFminus.csv.bz2", header = FALSE, stringsAsFactors= FALSE)  
 
 colnames(esd0) <- c("feature",c(1:933))
-colnames(esd2) <- c("feature",c(934:1236))
-colnames(esd4) <- c("feature",c(1237:1919))
 colnames(esd7) <- c("feature",c(1920:2717))
 
-counts <- merge (esd0,esd2,by="feature",all=T)
-counts <- merge (counts, esd4, by="feature", all=T)
-counts <- merge (counts, esd7, by="feature", all=T)
+counts <- merge (esd0,esd7,by="feature",all=T)
 rownames(counts) <- counts$feature
 counts <- counts[,!(colnames(counts) %in% "feature")]
 
-celltype<- c(rep("d0",933),rep("d2", 303),rep("d4", 683),rep("d7" , 798))
+celltypeCol <- c(rep("red",933),rep("blue",798))
+celltype <- c(rep("d0",933),rep("d7",798))
 
 # ---- Gene-Filter ----
 # Remove lowly expressed Genes
@@ -50,29 +43,66 @@ featuresHE <- featureCalc(countsHE, htseq = FALSE, minExpr)
 
 
 # ---- Size-Factors ----
+
 #Normal SF
 geoMeans_HE <- apply(countsHE, 1, function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0]))))
 szf_HE <- estimateSizeFactorsForMatrix(countsHE,geoMeans = geoMeans_HE)
 
-#SF For AK method
-szf_al <- normalizeBySums(countsHE)
+#Normal TMM
+tmm <- calcNormFactors(as.matrix(countsHE),method="TMM") * colSums(countsHE)
 
-#SF for AK with Cluster
-szfCluster <- quickCluster(countsHE)
-szf_alClust <- normalizeBySums(countsHE, clusters = szfCluster)
+#Normal Library Size
+libfactor <- colSums(countsHE) / 10000
+
+#Deconvoluted
+szfCluster <- quickCluster(countsHE,deepSplit = 1)
+szf_alClust <- normalizeBySums(countsHE,cluster=szfCluster)
+
+# ---- Normalize ----
+
+countsHENorm <- as.data.frame(t(t(countsHE) / szf_HE))
+countsHENorm_alClust <- as.data.frame(t(t(countsHE) / szf_alClust))
 
 
 # ---- SF-Difference ----
+scaled_factors<- data.frame("Sizefactor" = szf_HE / median(szf_HE),"TMM" = tmm/median(tmm), "LibrarySize" = libfactor/median(libfactor), "Deconvoluted" = szf_alClust/median(szf_alClust))
+boxplot(scaled_factors,log="y",ylab="Scaled Normalizationfactor")
+plot(scaled_factors$Sizefactor,scaled_factors$Deconvoluted,cex=0.6,pch=19,col="#00000073",xlab="Sizefactor",ylab="Deconvoluted",xlim=c(0.1,3.5),ylim=c(0.1,3.5),log="xy")
+abline(0,1,col="dodgerblue")
 
-all_sizefactors <- data.frame("SF" = szf_HE,"SF_after_Sum." = szf_al, "SF_after_SumClust" = szf_alClust)
-boxplot(all_sizefactors,main="Distribution of all SF")
-plot(szf_HE,szf_al,main="SF vs SF After Summation",xlab="Normal SF",ylab="SF after Summation",cex=0.6,pch=19,col="#00000073")
-plot(szf_HE,szf_alClust,main="SF vs SF After Summation and Clustering",xlab="Normal SF",ylab="SF after Summation and Clustering",cex=0.6,pch=19,col="#00000073")
-plot(szf_al,szf_alClust,main="SF After Summation vs SF After Summation and Clustering",xlab="SF after Summation",ylab="SF after Summation and Clustering",cex=0.6,pch=19,col="#00000073")
+plot(scaled_factors$LibrarySize,scaled_factors$Deconvoluted,cex=0.6,pch=19,col="#00000073",xlab="Librarysize",ylab="Deconvoluted",xlim=c(0.1,8),ylim=c(0.1,8),log="xy")
+abline(0,1,col="dodgerblue")
 
-# ---- Normalize ----
-countsHENorm <- as.data.frame(t(t(countsHE) / szf_HE))
-countsHENorm_al <- as.data.frame(t(t(countsHE) / szf_al))
+# ---- Diferential-Expression ----
 
-featuresNorm <- featureCalc(countsHENorm, htseq = FALSE, exprmin = 1)
-featuresNorm_al <- featureCalc(countsHENorm_al, htseq = FALSE, exprmin = 1)
+#Construct colData for both Sets
+colData_szf <- data.frame("Cells" = celltype, "sizeFactor" = szf_HE)
+colData_szf_alClust <- data.frame("Cells" = celltype, "sizeFactor" = szf_alClust)
+colData_lib <- data.frame("Cells" = celltype, "sizeFactor" = libfactor)
+colData_tmm <- data.frame("Cells" = celltype, "sizeFactor" = tmm)
+
+
+## Load into DESeq2 ! Careful takes a lot of time !
+dds_szf<- DESeqDataSetFromMatrix(countData = countsHE, colData = colData_szf, design = ~ Cells)
+dds_szf <- estimateDispersions(dds_szf)
+dds_szf <- nbinomWaldTest(dds_szf)
+rslt_szf <- results(dds_szf)
+write.csv(as.data.frame(rslt_szf),"Klein_Results_SF.csv")
+
+dds_szf_al <- DESeqDataSetFromMatrix(countData = countsHE, colData =colData_szf_alClust, design = ~ Cells)
+dds_szf_al <- estimateDispersions(dds_szf_al)
+dds_szf_al <- nbinomWaldTest(dds_szf_al)
+rslt_szf_al <- results(dds_szf_al)
+write.csv(as.data.frame(rslt_szf_al),"Klein_Results_Decon.csv")
+
+dds_lib <- DESeqDataSetFromMatrix(countData = countsHE, colData =colData_lib, design = ~ Cells)
+dds_lib <- estimateDispersions(dds_lib)
+dds_lib <- nbinomWaldTest(dds_lib)
+rslt_lib <- results(dds_lib)
+write.csv(as.data.frame(rslt_lib),"Klein_Results_Lib.csv")
+
+dds_tmm <- DESeqDataSetFromMatrix(countData = countsHE, colData =colData_tmm, design = ~ Cells)
+dds_tmm <- estimateDispersions(dds_tmm)
+dds_tmm <- nbinomWaldTest(dds_tmm)
+rslt_tmm <- results(dds_tmm)
+write.csv(as.data.frame(rslt_tmm),"Klein_Results_TMM.csv")
--- realdata/Zeisel.R
+++ realdata/Zeisel.R
@@ -1,4 +1,4 @@
-## AK47 Normalization
+# AK47 Normalization
 
 # ---- Packages ----
 pkgs <- "/nfs/research2/marioni/Karsten/utils/Rpack/"
@@ -10,6 +10,7 @@ library('zoo',lib.loc=pkgs)
 library('tsne',lib.loc=pkgs)
 library('DESeq2')
 library('plyr')
+library('edgeR')
 library('dynamicTreeCut',lib.loc=pkgs)
 library('moduleColor',lib.loc=pkgs)
 library('lattice',lib.loc=pkgs)
@@ -62,7 +63,6 @@ mitcounts <- mitcounts[,colnames(counts)]
 # Combine data sets into one expression matrix 
 counts <- rbind(counts,mitcounts,spikecounts)
 
-# Create a neuronset and a normal set
 
 minExpr <- 1
 
@@ -80,148 +80,145 @@ featuresHE <- featureCalc(countsHE, htseq = FALSE, minExpr)
 
 # ---- Size-Factors ----
 countsHEnoSpike <- countsHE[!featuresHE$spike,]
+
 #Normal SF
 geoMeans_HE <- apply(countsHEnoSpike, 1, function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0]))))
 szf_HE <- estimateSizeFactorsForMatrix(countsHEnoSpike,geoMeans = geoMeans_HE)
 
-#SF For AK method
-szf_al <- normalizeBySums(countsHEnoSpike)
+#Normal TMM
+tmm <- calcNormFactors(as.matrix(countsHEnoSpike),method="TMM") * colSums(countsHEnoSpike)
 
-#SF for AK with Cluster
+#Normal Library Size
+libfactor <- colSums(countsHEnoSpike) / 10000
+
+#ERCC Size factors
+erccfactor <- colSums(countsHE[featuresHE$spike,]) / 1000
+
+#Deconvoluted
 szfCluster <- quickCluster(countsHEnoSpike)
 szf_alClust <- normalizeBySums(countsHEnoSpike, clusters = szfCluster)
 
 
 # ---- SF-Difference ----
 
-all_sizefactors <- data.frame("SF" = szf_HE,"SF_after_Sum." = szf_al, "SF_after_SumClust" = szf_alClust)
-boxplot(all_sizefactors,main="Distribution of all SF")
-plot(szf_HE,szf_al,main="SF vs SF After Summation",xlab="Normal SF",ylab="SF after Summation",cex=0.6,pch=19,col="#00000073")
-plot(szf_HE,szf_alClust,main="SF vs SF After Summation and Clustering",xlab="Normal SF",ylab="SF after Summation and Clustering",cex=0.6,pch=19,col="#00000073")
-plot(szf_al,szf_alClust,main="SF After Summation vs SF After Summation and Clustering",xlab="SF after Summation",ylab="SF after Summation and Clustering",cex=0.6,pch=19,col="#00000073")
+scaled_factors <- data.frame("Sizefactor" = szf_HE / median(szf_HE),"TMM" = tmm/median(tmm), "LibrarySize" = libfactor/median(libfactor), "Deconvoluted" = szf_alClust/median(szf_alClust))
+#Comparison of SF Distribution
+boxplot(scaled_factors,log="y",ylab="Scaled Normalizationfactor")
+
+#SF vs Deconvoluted
+plot(scaled_factors$Sizefactor,scaled_factors$Deconvoluted,xlim=c(0.1,6),ylim=c(0.1,6),cex=0.6,pch=19,col="#00000073",xlab="Sizefactor",ylab="Deconvoluted",log="xy")
+abline(0,1,col="dodgerblue")
+
+#Libfactor vs Deconvoluted
+plot(scaled_factors$LibrarySize,scaled_factors$Deconvoluted,xlim=c(0.1,7),ylim=c(0.1,7),cex=0.6,pch=19,col="#00000073",xlab="Librarysize",ylab="Deconvoluted",log="xy")
+abline(0,1,col="dodgerblue")
+
+#TMM vs Deconvoluted
+plot(scaled_factors$TMM,scaled_factors$Deconvoluted,xlim=c(0.1,7),ylim=c(0.1,7),cex=0.6,pch=19,col="#00000073",xlab="TMM",ylab="Deconvoluted")
+abline(0,1,col="dodgerblue")
 
 # ---- Normalize ----
-countsHENorm <- as.data.frame(t(t(countsHEnoSpike) / szf_HE))
-countsHENorm_al <- as.data.frame(t(t(countsHEnoSpike) / szf_al))
 
-featuresNorm <- featureCalc(countsHENorm, htseq = FALSE, exprmin = 1)
-featuresNorm_al <- featureCalc(countsHENorm_al, htseq = FALSE, exprmin = 1)
+# countsHENorm <- as.data.frame(t(t(countsHEnoSpike) / szf_HE))
+# countsHENorm_alClust <- as.data.frame(t(t(countsHEnoSpike) / szf_alClust))
 
-# ---- Evaluate-Noise ----
-# spike_colNorm <- as.numeric(featuresNorm$spike) + 1
-# plot(featuresNorm$mean,featuresNorm$cv2,log="xy",cex=0.3,col=spike_colNorm,pch=19,main="Not Summed")
-# curve((sqrt(x)/x)^2,from=0.001,to=1000,add = T, col = "red")
-# 
-# spike_colNorm_al <- as.numeric(featuresNorm_al$spike) + 1
-# plot(featuresNorm_al$mean,featuresNorm_al$cv2,log="xy",cex=0.3,col=spike_colNorm_al,pch=19,main="Summed method")
-# curve((sqrt(x)/x)^2,from=0.001,to=1000,add = T, col = "red")
-# dev.off()
-# 
-# dm <- DM(meanGenes=featuresHE$mean,CV2Genes=featuresHE$cv2)
-# dmNorm <- DM(meanGenes=featuresNorm$mean,CV2Genes=featuresNorm$cv2)
-# dmNorm_al <- DM(meanGenes=featuresNorm_al$mean,CV2Genes=featuresNorm_al$cv2)
-# 
-# dmERCC <- dm[grepl("ERCC",rownames(countsHE))]
-# dmERCC_norm <- dmNorm[grepl("ERCC",rownames(countsHE))]
-# dmERCC_al <- dmNorm_al[grepl("ERCC",rownames(countsHE))]
-# 
-# boxplot(data.frame(dmERCC,dmERCC_norm,dmERCC_al),main="Boxplot of DM of ERCCs")
-
-# ---- ExtremeSFValues ----
-
-# par(mfrow=c(1,2))
-# plot(szf_al,t(nzeros)[,1]/median(t(nzeros)[,1]),xlab="Sizefactors After Summation",ylab="Relative Amount of Zeros")
-# plot(szf_HE,t(nzeros)[,1]/median(t(nzeros)[,1]),xlab="Sizefactors",ylab="Relative Amount of Zeros")
-
-plot(szf_al,colSums(countsHE),xlab="Sizefactors After Summation",ylab="Total Lib.Size",cex=0.6,pch=19,col="#00000073")
-plot(szf_HE,colSums(countsHE),xlab="Sizefactors",ylab="Total Lib.Size",cex=0.6,pch=19,col="#00000073")
-
-# ---- Sizefactor-Information ----
-
-plot(szf_HE,colSums(countsHE[!featuresHE$spike,])/colSums(countsHE[featuresHE$spike,]),ylab="Cellsize as Cellreads / ERCCreads",xlab="Sizefactors",cex=0.6,pch=19,col="#00000073",main="Cellsize vs. SF")
-plot(szf_al,colSums(countsHE[!featuresHE$spike,])/colSums(countsHE[featuresHE$spike,]),ylab="Cellsize as Cellreads / ERCCreads",xlab="Sizefactors after Summation",cex=0.6,pch=19,col="#00000073",main="Cellsize vs SF after Sum")
-
-plot(szf_HE,colSums(countsHE),ylab="Total Counts",xlab="Sizefactors",cex=0.6,pch=19,col="#00000073")
-plot(szf_al,colSums(countsHE),ylab="Total Counts",xlab="Sizefactors after Summation",cex=0.6,pch=19,col="#00000073")
-
+# ---- LibrarySize ----
+# Librarysizes <- c(colSums(countsHE),colSums(countsHENorm),colSums(countsHENorm_alClust))
+# Normalization <- c(rep("Raw",ncol(countsHE)),rep("SF",ncol(countsHENorm)),rep("SF_al",ncol(countsHENorm_alClust)))
+# ddf <- data.frame(Librarysizes,Normalization)
+# plt <- ggplot(ddf, aes(x=Librarysizes, fill=Normalization, group=Normalization))
+# plot(plt + geom_density(alpha=0.3) + scale_x_log10())
 # ---- Spike-Ins ----
 
-plot(szf_HE,colSums(countsHE[featuresHE$spike,]),ylab=" ERCCreads",xlab="Sizefactors",log="x",cex=0.6,pch=19,col="#00000073")
-plot(szf_al,colSums(countsHE[featuresHE$spike,]),ylab=" ERCCreads",xlab="Sizefactors after Summation",log="x",cex=0.6,pch=19,col="#00000073")
+# plot(scaled_factors$Sizefactor,erccfactor/median(erccfactor),ylab="Spike-In Sizefactor",xlab="Sizefactors",log="x",cex=0.6,pch=19,col="#00000073")
+# plot(scaled_factors$TMM,erccfactor/median(erccfactor),ylab="Spike-In Sizefactor",xlab="TMM",log="x",cex=0.6,pch=19,col="#00000073")
+# plot(scaled_factors$Deconvoluted,erccfactor/median(erccfactor),ylab="Spike-In Sizefactor",xlab="Deconvoluted",log="x",cex=0.6,pch=19,col="#00000073")
+# plot(scaled_factors$LibrarySize,erccfactor/median(erccfactor),ylab="Spike-In Sizefactor",xlab="LibrarySize",log="x",cex=0.6,pch=19,col="#00000073")
 
 ## Compute conversionFactor
-betafactor <- apply(countsHE[featuresHE$spike,],2, function(x) lm(x ~ molecules[molecules$ERCC_ID %in% rownames(featuresHE)[featuresHE$spike],"molecules_in_each_chamber"])$coefficients[2])
+# betafactor <- apply(countsHE[featuresHE$spike,],2, function(x) lm(x ~ molecules[molecules$ERCC_ID %in% rownames(featuresHE)[featuresHE$spike],"molecules_in_each_chamber"])$coefficients[2])
 
 ## Sizefactors dont correspond to conversion factors
-plot(szf_HE,betafactor,cex=0.6,pch=19,col="#00000073",xlab="Sizefactors",ylab="Conversion Factors",main="Sizefactors are not assoicated with Conversion Factors")
+# plot(szf_HE,betafactor,cex=0.6,pch=19,col="#00000073",xlab="Sizefactors",ylab="Conversion Factors",main="Sizefactors are not assoicated with Conversion Factors")
 
 # ---- Effect-on-HVG ---
 
 ##Order of Variability
-plot(order(dm),order(dmNorm),cex=0.5,pch=19)
-plot(order(dm),order(dmNorm_al),cex=0.5,pch=19)
-plot(order(dmNorm),order(dmNorm_al),cex=0.5,pch=19)
-
-featuresHE_Ord<- featuresHE[order(-dm),]
-featuresNorm_Ord<- featuresNorm[order(-dmNorm),]
-featuresNorm_alOrd <- featuresNorm_al[order(-dmNorm_al),]
-
-featuresHE$hvg <- rownames(featuresHE) %in% rownames(featuresHE_Ord)[1:1000]
-featuresNorm$hvg <- rownames(featuresNorm) %in% rownames(featuresNorm_Ord)[1:1000]
-featuresNorm_al$hvg <- rownames(featuresNorm_al) %in% rownames(featuresNorm_alOrd)[1:1000]
-
-venHE <- which(featuresHE$hvg)
-venNorm <- which(featuresNorm$hvg)
-venNorm_al <- which(featuresNorm_al$hvg)
-
-venn.diagram(list("Non-Norm" = venHE,"Sizefacotrs" = venNorm, "Summation" = venNorm_al), fill = c("red","green","blue"),
-             alpha= c(0.5,0.5,0.5), cex = 1 , cat.fontface = 4, fontfamily = 3,imagetype="png",
-             filename = "VenDiagram2.png")
+# plot(order(dm),order(dmNorm),cex=0.5,pch=19)
+# plot(order(dm),order(dmNorm_al),cex=0.5,pch=19)
+# plot(order(dmNorm),order(dmNorm_al),cex=0.5,pch=19)
+# 
+# featuresHE_Ord<- featuresHE[order(-dm),]
+# featuresNorm_Ord<- featuresNorm[order(-dmNorm),]
+# featuresNorm_alOrd <- featuresNorm_al[order(-dmNorm_al),]
+# 
+# featuresHE$hvg <- rownames(featuresHE) %in% rownames(featuresHE_Ord)[1:1000]
+# featuresNorm$hvg <- rownames(featuresNorm) %in% rownames(featuresNorm_Ord)[1:1000]
+# featuresNorm_al$hvg <- rownames(featuresNorm_al) %in% rownames(featuresNorm_alOrd)[1:1000]
+# 
+# venHE <- which(featuresHE$hvg)
+# venNorm <- which(featuresNorm$hvg)
+# venNorm_al <- which(featuresNorm_al$hvg)
+# 
+# venn.diagram(list("Non-Norm" = venHE,"Sizefacotrs" = venNorm, "Summation" = venNorm_al), fill = c("red","green","blue"),
+#              alpha= c(0.5,0.5,0.5), cex = 1 , cat.fontface = 4, fontfamily = 3,imagetype="png",
+#              filename = "VenDiagram2.png")
 
 # ---- Diferential-Expression ----
 
-sbst <- countsHE[,grepl("microglia|oligodendrocytes",cells$level1class)]
+sbst <- countsHEnoSpike[,grepl("microglia|oligodendrocytes",cells$level1class)]
 cellssbst <- cells[grepl("microglia|oligodendrocytes",cells$level1class),]
 cellssbst$sizeFactor <- szf_HE[grepl("microglia|oligodendrocytes",cells$level1class)]
 
 cellssbst_al <- cells[grepl("microglia|oligodendrocytes",cells$level1class),]
-cellssbst_al$sizeFactor <- szf_al[grepl("microglia|oligodendrocytes",cells$level1class)]
+cellssbst_al$sizeFactor <- szf_alClust[grepl("microglia|oligodendrocytes",cells$level1class)]
 
+cellssbst_tmm <- cells[grepl("microglia|oligodendrocytes",cells$level1class),]
+cellssbst_tmm$sizeFactor <- tmm[grepl("microglia|oligodendrocytes",cells$level1class)]
 
-## Load into DESeq2 ! Careful takes a lot of time !
+cellssbst_lib<- cells[grepl("microglia|oligodendrocytes",cells$level1class),]
+cellssbst_lib$sizeFactor <- libfactor[grepl("microglia|oligodendrocytes",cells$level1class)]
+
+#DESeq for SF
 dds_sbst <- DESeqDataSetFromMatrix(countData = sbst, colData =cellssbst, design =~ level1class)
 dds_sbst <- estimateDispersions(dds_sbst)
 dds_sbst <- nbinomWaldTest(dds_sbst)
 rslt_sbst <- results(dds_sbst)
+write.csv(as.data.frame(rslt_sbst),"Zeisel_Result_SF.csv")
 
+#DESeq for Deconvoluted
 dds_sbst_al <- DESeqDataSetFromMatrix(countData = sbst, colData =cellssbst_al, design =~ level1class)
 dds_sbst_al <- estimateDispersions(dds_sbst_al)
 dds_sbst_al <- nbinomWaldTest(dds_sbst_al)
-
 rslt_sbst_al <- results(dds_sbst_al)
+write.csv(as.data.frame(rslt_sbst_al),"Zeisel_Result_Deconv.csv")
 
-up_sbst <- rownames(rslt_sbst)[rslt_sbst$log2FoldChange > 0 & rslt_sbst$padj < 0.1]
-up_sbst_al <- rownames(rslt_sbst_al)[rslt_sbst_al$log2FoldChange > 0 & rslt_sbst_al$padj < 0.1]
+#DESeq for TMM
+dds_sbst_tmm <- DESeqDataSetFromMatrix(countData = sbst, colData =cellssbst_tmm, design =~ level1class)
+dds_sbst_tmm <- estimateDispersions(dds_sbst_tmm)
+dds_sbst_tmm <- nbinomWaldTest(dds_sbst_tmm)
+rslt_sbst_tmm <- results(dds_sbst_tmm)
+write.csv(as.data.frame(rslt_sbst_tmm),"Zeisel_Result_TMM.csv")
 
-down_sbst <- rownames(rslt_sbst)[rslt_sbst$log2FoldChange < 0 & rslt_sbst$padj < 0.1]
-down_sbst_al <- rownames(rslt_sbst_al)[rslt_sbst_al$log2FoldChange < 0 & rslt_sbst$padj < 0.1]
+#DESeq for Libfactor
+dds_sbst_lib <- DESeqDataSetFromMatrix(countData = sbst, colData =cellssbst_lib, design =~ level1class)
+dds_sbst_lib <- estimateDispersions(dds_sbst_lib)
+dds_sbst_lib <- nbinomWaldTest(dds_sbst_lib)
+rslt_sbst_lib <- results(dds_sbst_lib)
+write.csv(as.data.frame(rslt_sbst_lib),"Zeisel_Result_Lib.csv")
 
-int_up <- intersect(up_sbst,up_sbst_al)
-int_down <- intersect(down_sbst, down_sbst_al)
+# up_sbst <- rownames(rslt_sbst)[rslt_sbst$log2FoldChange > 0 & rslt_sbst$padj < 0.1]
+# up_sbst_al <- rownames(rslt_sbst_al)[rslt_sbst_al$log2FoldChange > 0 & rslt_sbst_al$padj < 0.1]
+# 
+# down_sbst <- rownames(rslt_sbst)[rslt_sbst$log2FoldChange < 0 & rslt_sbst$padj < 0.1]
+# down_sbst_al <- rownames(rslt_sbst_al)[rslt_sbst_al$log2FoldChange < 0 & rslt_sbst$padj < 0.1]
+# 
+# int_up <- intersect(up_sbst,up_sbst_al)
+# int_down <- intersect(down_sbst, down_sbst_al)
+# 
+# int_up_down <- intersect(up_sbst,down_sbst_al)
+# int_down_up <- intersect(down_sbst,up_sbst_al)
+# 
+# barplot(c(length(up_sbst),length(up_sbst_al),length(int_up),length(down_sbst),length(down_sbst_al),length(int_down),length(int_up_down),length(int_down_up)),width=0.5,names.arg=c("SF","SF_AS","Overlap","SF","SF_AS","Overlap","Overlap","Overlap"),col=c("#FB6A4A","#FB6A4A","#EF3B2C","#6BAED6","#6BAED6","#2171B5","seagreen2","tomato2"),cex.names=0.7,space=c(0.1,0.1,0.1,0.5,0.1,0.1,0.5,0.1),main="Oligodendrocyte v. Microglia")
+# legend("topright",fill=c("#FB6A4A","#6BAED6","seagreen2","tomato2"),legend=c("Upregulated","Downregulated","Down after Sum","Up after Sum"))
 
-int_up_down <- intersect(up_sbst,down_sbst_al)
-int_down_up <- intersect(down_sbst,up_sbst_al)
-
-barplot(c(length(up_sbst),length(up_sbst_al),length(int_up),length(down_sbst),length(down_sbst_al),length(int_down),length(int_up_down),length(int_down_up)),width=0.5,names.arg=c("SF","SF_AS","Overlap","SF","SF_AS","Overlap","Overlap","Overlap"),col=c("#FB6A4A","#FB6A4A","#EF3B2C","#6BAED6","#6BAED6","#2171B5","seagreen2","tomato2"),cex.names=0.7,space=c(0.1,0.1,0.1,0.5,0.1,0.1,0.5,0.1),main="Oligodendrocyte v. Microglia")
-legend("topright",fill=c("#FB6A4A","#6BAED6","seagreen2","tomato2"),legend=c("Upregulated","Downregulated","Down after Sum","Up after Sum"))
-
-
-mat <- countsHENorm[na.omit(int_up_down[1:7]),grepl("microglia|oligodendrocytes",cells$level1class)]
-mat <- log(mat +1)
-dfVar <- as.data.frame(colData(dds_sbst)[,"level1class"])
-pheatmap(mat, annotation_col=dfVar,cluster_cols = FALSE)
-
-mat_al <- countsHENorm_al[na.omit(int_up_down[1:7]),grepl("microglia|oligodendrocytes",cells$level1class)]
-mat_al <- log(mat_al +1)
-dfVar_al <- as.data.frame(colData(dds_sbst_al)[,"level1class"])
-pheatmap(mat_al, annotation_col=dfVar_al,cluster_cols=FALSE)
