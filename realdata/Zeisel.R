d778d099b193717ebc9620415eec8e152e99c7f3
Karsten Bach <karsten_bach@hotmail.com>
2015-12-17 16:50:01 +0000

    Added HVG analysis

--- realdata/Zeisel.R
+++ realdata/Zeisel.R
@@ -5,9 +5,6 @@ pkgs <- "/nfs/research2/marioni/Karsten/utils/Rpack/"
 library('rJava',lib.loc=pkgs)
 library('xlsxjars',lib.loc=pkgs)
 library('xlsx',lib.loc=pkgs)
-library('VennDiagram',lib.loc=pkgs)
-library('zoo',lib.loc=pkgs)
-library('tsne',lib.loc=pkgs)
 library('DESeq2')
 library('plyr')
 library('edgeR')
@@ -16,8 +13,6 @@ library('moduleColor',lib.loc=pkgs)
 library('lattice',lib.loc=pkgs)
 library('ggplot2',lib.loc=pkgs)
 library('gridExtra',lib.loc=pkgs)
-library('pheatmap',lib.loc=pkgs)
-library("RColorBrewer")
 library('AK47')
 source("./functions.R") 
 source("./DM.R")
@@ -83,7 +78,8 @@ countsHEnoSpike <- countsHE[!featuresHE$spike,]
 
 #Normal SF
 geoMeans_HE <- apply(countsHEnoSpike, 1, function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0]))))
-szf_HE <- estimateSizeFactorsForMatrix(countsHEnoSpike,geoMeans = geoMeans_HE)
+
+szf_HE <- estimateSizeFactorsForMatrix(countsHEnoSpike, geoMeans = geoMeans_HE)
 
 #Normal TMM
 tmm <- calcNormFactors(as.matrix(countsHEnoSpike),method="TMM") * colSums(countsHEnoSpike)
@@ -98,13 +94,18 @@ erccfactor <- colSums(countsHE[featuresHE$spike,]) / 1000
 szfCluster <- quickCluster(countsHEnoSpike)
 szf_alClust <- normalizeBySums(countsHEnoSpike, clusters = szfCluster)
 
-
 # ---- SF-Difference ----
-
-scaled_factors <- data.frame("DESeq" = szf_HE / median(szf_HE),"TMM" = tmm/median(tmm), "Library size" = libfactor/median(libfactor[cells$group != "4"]), "Deconvolution" = szf_alClust/median(szf_alClust[cells$group != "4"]),check.names=FALSE)
+set.seed(1)
+shffl <- sample(ncol(countsHE))
+scaled_factors <- data.frame("DESeq" = (szf_HE/median(szf_HE))[shffl],"TMM" = (tmm/median(tmm))[shffl], "Library size" = (libfactor/median(libfactor))[shffl], "Deconvolution" = (szf_alClust/median(szf_alClust))[shffl],check.names=FALSE)
 #Comparison of SF Distribution
-cellCols <- labels2colors(as.integer(cells$group))
-cellCols2 <- sapply(cells$group, function(x) if (x == "4") "firebrick1" else "#00000073")
+#Colors
+line.col <- "red"
+olig.col <- rgb(1,0.5,0)
+pyr.col <- "dodgerblue"
+cell.col <- rgb(0,0,0) 
+
+setCols <- sapply(cells$level1class[shffl], function(x) if (x == "oligodendrocytes") olig.col  else if (x == "pyramidal CA1") pyr.col else cell.col)
 
 cairo_pdf("Zeisel_NormFactors.pdf")
 par(mar=c(8.6,5.1,2.1,1.1))
@@ -115,38 +116,39 @@ dev.off()
 
 cairo_pdf("Zeisel_SFvDeconv.pdf")
 par(mar=c(5.1,5.1,4.1,1.1))
-plot(scaled_factors$DESeq,scaled_factors$Deconvolution,xlim=c(0.1,6),ylim=c(0.1,6),cex=0.6,pch=19,col="#00000073",xlab="DESeq",ylab="Deconvolution",log="xy",cex.axis=1.5,cex.lab=1.8)
-abline(0,1,col="dodgerblue")
+plot(scaled_factors$DESeq,scaled_factors$Deconvolution,xlim=c(0.1,6),ylim=c(0.1,6),pch=16,xlab="DESeq",ylab="Deconvolution",log="xy",cex.axis=1.5,cex.lab=1.8,col=setCols)
+abline(0,1,col=line.col,lwd=2)
 dev.off()
 
 #Libfactor vs Deconvoluted
 cairo_pdf("Zeisel_LibvDeconv.pdf")
 par(mar=c(5.1,5.1,4.1,1.1))
-plot(libfactor/median(libfactor[cells$group != "4"]),szf_alClust/median(szf_alClust[cells$group != "4"]),xlim=c(0.1,7),ylim=c(0.1,7),cex=0.6,pch=19,xlab="Library size",ylab="Deconvolution",log="xy",cex.axis=1.5,cex.lab=1.8,col=cellCols2)
-abline(0,1,col="dodgerblue")
+plot(scaled_factors[,"Library size"],scaled_factors$Deconvolution,xlim=c(0.1,7),ylim=c(0.1,7),pch=16,xlab="Library size",ylab="Deconvolution",log="xy",cex.axis=1.5,cex.lab=1.8,col=setCols)
+abline(0,1,col=line.col,lwd=2)
 dev.off()
 
 #TMM vs Deconvoluted
 cairo_pdf("Zeisel_TMMvDeconv.pdf")
 par(mar=c(5.1,5.1,4.1,1.1))
-plot(scaled_factors$TMM,scaled_factors$Deconvolution,xlim=c(0.1,7),ylim=c(0.1,7),cex=0.6,pch=19,col="#00000073",xlab="TMM",ylab="Deconvolution",cex.axis=1.5,cex.lab=1.8,log="xy")
-abline(0,1,col="dodgerblue")
+plot(scaled_factors$TMM,scaled_factors$Deconvolution,xlim=c(0.1,7),ylim=c(0.1,7),pch=16,col=setCols,xlab="TMM",ylab="Deconvolution",cex.axis=1.5,cex.lab=1.8,log="xy")
+abline(0,1,col=line.col,lwd=2)
 dev.off()
-# ---- Normalize ----
 
-# countsHENorm <- as.data.frame(t(t(countsHEnoSpike) / szf_HE))
-# countsHENorm_alClust <- as.data.frame(t(t(countsHEnoSpike) / szf_alClust))
+# LFC in sizefactors between sziefactor and deconvolution for the a) all b) pyramidal c) oligodendrocytes
+# scaled_lib <- libfactor/median(libfactor[cells$group != "4"])
+# scaled_szf <- szf_alClust/median(szf_alClust[cells$group != "4"])
+# 
+# lfc_al <- log2(scaled_lib/scaled_szf)
+# lfc_oligo <- log2(scaled_lib[cells$group == "4"] / scaled_szf[cells$group == "4"])
+# lfc_pyramidal <- log2(scaled_lib[cells$group == "3"] / scaled_szf[cells$group == "3"])
+# lfc_micro <- log2(scaled_lib[cells$group == "5"] / scaled_szf[cells$group == "5"])
+# 
+# boxplot(lfc_al,lfc_oligo,lfc_pyramidal,lfc_micro)
 
-# ---- LibrarySize ----
-# Librarysizes <- c(colSums(countsHE),colSums(countsHENorm),colSums(countsHENorm_alClust))
-# Normalization <- c(rep("Raw",ncol(countsHE)),rep("SF",ncol(countsHENorm)),rep("SF_al",ncol(countsHENorm_alClust)))
-# ddf <- data.frame(Librarysizes,Normalization)
-# plt <- ggplot(ddf, aes(x=Librarysizes, fill=Normalization, group=Normalization))
-# plot(plt + geom_density(alpha=0.3) + scale_x_log10())
 # ---- Spike-Ins ----
 cairo_pdf("Zeisel_ERCCvSF.pdf")
 par(mar=c(5.1,5.1,4.1,1.1))
-plot(scaled_factors$DESeq,erccfactor/median(erccfactor),ylab="Spike-in",xlab="DESeq",log="x",cex=0.6,pch=19,col="#00000073",cex.axis=1.5,cex.lab=1.8)
+plot(scaled_factors$DESeq,erccfactor/median(erccfactor),ylab="Spike-in",xlab="DESeq",log="x",pch=16,col="#00000073",cex.axis=1.5,cex.lab=1.8)
 dev.off()
 cairo_pdf("Zeisel_ERCCvTMM.pdf")
 par(mar=c(5.1,5.1,4.1,1.1))
@@ -161,66 +163,30 @@ par(mar=c(5.1,5.1,4.1,1.1))
 plot(scaled_factors[,"Library size"],erccfactor/median(erccfactor),ylab="Spike-in",xlab="Library size",log="x",cex=0.6,pch=19,col="#00000073",cex.axis=1.5,cex.lab=1.8)
 dev.off()
 
+# ---- Normalization ----
 
-# ---- Diferential-Expression ----
-## Subset Data for EdgeR
-countssbst <- countsHEnoSpike[,grepl("microglia|oligodendrocytes",cells$level1class)]
-cellssbst <- cells[grepl("microglia|oligodendrocytes",cells$level1class),]
+counts.sf <- as.data.frame(t(t(countsHEnoSpike) / szf_HE))
+counts.tmm <- as.data.frame(t(t(countsHEnoSpike) / tmm))
+counts.lib <- as.data.frame(t(t(countsHEnoSpike) / libfactor))
+counts.decon <- as.data.frame(t(t(countsHEnoSpike) / szf_alClust))
 
-szf_sbst<- szf_HE[grepl("microglia|oligodendrocytes",cells$level1class)]
+features.sf <- featureCalc(counts.sf, htseq=FALSE,minExpr)
+features.tmm <- featureCalc(counts.tmm, htseq=FALSE,minExpr)
+features.lib <- featureCalc(counts.lib, htseq=FALSE,minExpr)
+features.decon <- featureCalc(counts.decon, htseq=FALSE,minExpr)
 
-szf_sbst_al<- szf_alClust[grepl("microglia|oligodendrocytes",cells$level1class)]
+# ---- HVG-calling ----
 
-tmm_sbst<- tmm[grepl("microglia|oligodendrocytes",cells$level1class)]
+dm.sf <- DM(meanGenes=features.sf$mean,CV2Genes=features.sf$cv2)
+dm.tmm <- DM(meanGenes=features.tmm$mean,CV2Genes=features.tmm$cv2)
+dm.lib <- DM(meanGenes=features.lib$mean,CV2Genes=features.lib$cv2)
+dm.decon <- DM(meanGenes=features.decon$mean,CV2Genes=features.decon$cv2)
 
-lib_sbst <- libfactor[grepl("microglia|oligodendrocytes",cells$level1class)]
+features.sfOrd <- features.sf[order(-dm.sf),]
+features.tmmOrd <- features.tmm[order(-dm.tmm),]
+features.libOrd <- features.lib[order(-dm.lib),]
+features.deconOrd <- features.decon[order(-dm.decon),]
 
-# Setup EdgeR
-desgn <- model.matrix(~factor(cellssbst$level1class))
-y <- DGEList(countssbst)
+HVGlist <- list("DESeq"=rownames(features.sfOrd)[1:1000], "TMM"=rownames(features.tmmOrd)[1:1000], "Lib"=rownames(features.libOrd)[1:1000], "Deconvolution"=rownames(features.deconOrd)[1:1000])
 
-#Sizefactors
-y.sf <- y
-y.sf$samples$norm.factors <- szf_sbst/y.sf$samples$lib.size
-y.sf <- estimateDisp(y.sf,desgn)
-fit.sf <- glmFit(y.sf,desgn)
-res.sf <- glmLRT(fit.sf)
-
-#TMM
-y.tmm <- y
-y.tmm$samples$norm.factors <- tmm_sbst/y.tmm$samples$lib.size
-y.tmm <- estimateDisp(y.tmm,desgn)
-fit.tmm <- glmFit(y.tmm,desgn)
-res.tmm <- glmLRT(fit.tmm)
-
-#Library Size
-y.lib <- y
-y.lib$samples$norm.factors <- 1
-y.lib <- estimateDisp(y.lib,desgn)
-fit.lib <- glmFit(y.lib,desgn)
-res.lib <- glmLRT(fit.lib)
-
-#Deconvoluted
-y.al <- y
-y.al$samples$norm.factors <- szf_sbst_al/y.al$samples$lib.size
-y.al <- estimateDisp(y.al,desgn)
-fit.al <- glmFit(y.al,desgn)
-res.al <- glmLRT(fit.al)
-
-## Comparison
-
-x.sf <- decideTestsDGE(res.sf)
-x.lib <- decideTestsDGE(res.lib)
-x.tmm <- decideTestsDGE(res.tmm)
-x.al <- decideTestsDGE(res.al)
-
-
-out.file <- "Zeisel_output.txt"
-write.table(file=out.file, data.frame(Method="Size factor", Total=sum(x.sf!=0), Down=sum(x.sf<0), Up=sum(x.sf>0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
-write.table(file=out.file, data.frame(Method="TMM", Total=sum(x.tmm!=0), Down=sum(x.tmm<0), Up=sum(x.tmm>0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
-write.table(file=out.file, data.frame(Method="Library size", Total=sum(x.lib!=0), Down=sum(x.lib<0), Up=sum(x.lib>0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
-write.table(file=out.file, data.frame(Method="Deconvolution", Total=sum(x.al!=0), Down=sum(x.al<0), Up=sum(x.al>0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
-
-write.table(file=out.file, data.frame(Method="vs SF", Total=sum(x.sf!=0 & x.al!=0), Down=sum(x.sf<0 & x.al < 0), Up=sum(x.sf>0 & x.al > 0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
-write.table(file=out.file, data.frame(Method="vs TMM", Total=sum(x.tmm!=0 & x.al!=0), Down=sum(x.tmm<0 & x.al < 0), Up=sum(x.tmm>0 & x.al > 0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
-write.table(file=out.file, data.frame(Method="vs lib", Total=sum(x.lib!=0 & x.al!=0), Down=sum(x.lib<0 & x.al < 0), Up=sum(x.lib>0 & x.al > 0)), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE, append=TRUE)
+comparisonMatrix <- compareHVG(HVGlist)
