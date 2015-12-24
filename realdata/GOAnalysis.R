ff1191d1d68b6588c7ec54b1b0a6f65fc33a3ceb
Aaron Lun <alun@wehi.edu.au>
2015-12-24 13:05:45 +0000

    Cleaned up GO analysis script.

--- realdata/GOAnalysis.R
+++ realdata/GOAnalysis.R
@@ -14,15 +14,6 @@ sig.TMM <- TMM[TMM$FDR < 0.05,]
 sig.sf <- sf[sf$FDR < 0.05,]
 sig.decon <- decon[decon$FDR < 0.05,]
 
-
-# ---- biomart ----
-
-ensembl <- useMart(biomart= "ENSEMBL_MART_ENSEMBL", dataset="mmusculus_gene_ensembl", host ="www.ensembl.org")
-mm.genes <- getBM(attributes=c("ensembl_gene_id","external_gene_name"),mart=ensembl)
-
-geneConv <- mm.genes[mm.genes$external_gene_name %in% rownames(decon),]
-geneConv <- mm.genes[match(rownames(decon),mm.genes$external_gene_name),]
-
 # ---- GOanalysis ----
 
 ## testing unique DE genes
@@ -35,36 +26,33 @@ set.uq.lib <- as.integer(rownames(decon) %in% uq.lib)
 set.uq.TMM <- as.integer(rownames(decon) %in% uq.TMM)
 set.uq.sf <- as.integer(rownames(decon) %in% uq.sf)
 set.uq.decon <- as.integer(rownames(decon) %in% uq.decon)
+names(set.uq.lib) <- names(set.uq.TMM) <- names(set.uq.sf) <- names(set.uq.decon) <- rownames(decon)
 
 #prepare Data for topGO
 
 list.uq <- list("Lib"=set.uq.lib,"TMM"=set.uq.TMM,"SF"=set.uq.sf,"Decon"=set.uq.decon)
 ontologies <- c("BP","CC","MF")
 
-output <- list("Lib"=as.list(ontologies),"TMM"=as.list(ontologies),"DESeq"=as.list(ontologies),"Decon"=as.list(ontologies))
 output.dir <- "GOresults"
 dir.create(output.dir)
 
-
-for (i in 1:length(list.uq)) {
-
+for (i in seq_along(list.uq)) {
     alg <- factor(list.uq[[i]])
-    names(alg) <- geneConv$ensembl_gene_id
-    
-    for (x in ontologies) {
+    output <- list()
 
+    for (x in seq_along(ontologies)) {
         GO.data <- new("topGOdata",
-                      description="Lib GO",ontology=x,
+                      description="Lib GO",ontology=ontologies[x],
                       allGenes=alg, 
                       annot=annFUN.org, mapping="org.Mm.eg.db",
-                      nodeSize=10,ID="ensembl")
+                      nodeSize=10,ID="symbol")
 
         result.classic <- runTest(GO.data, algorithm="classic", statistic="Fisher" )
 
-        output[[i]][[x]] <- GenTable(GO.data, 
+        output[[x]] <- GenTable(GO.data, 
             Fisher.classic=result.classic,
             orderBy="Fisher.classic", topNodes = 200)
     }
-    output.print <- data.frame(output[i])[,-1:-3]
-    write.csv(output.print[,2:4],paste(output.dir,"/GOoutputUQin",names(list.uq)[i],".csv",sep=""))
+    output.print <- data.frame(output)[,-1:-3]
+    write.csv(output.print[,2:4], file.path(output.dir, paste0("GOoutputUQin", names(list.uq)[i], ".csv")))
 }
