ca89dfb894846dee97d70d13b5c9527dc0625103
Aaron Lun <alun@wehi.edu.au>
2016-01-24 16:14:47 +0000

    Reduced dispersions on low-count simulations.

--- simulations/saturated.sh
+++ simulations/saturated.sh
@@ -3,6 +3,7 @@ cat brittlesim.R | sed "s/true.means <- 2^runif(ngenes, 3, 6)/true.means <- 2^ru
 Rscript highcounts.R
 
 cat brittlesim.R | sed "s/true.means <- 2^runif(ngenes, 3, 6)/true.means <- 2^runif(ngenes, 0, 1)/" |
-                sed 's/output.dir <- "results"/output.dir <- "results_low"/' > lowcounts.R
+                sed 's/output.dir <- "results"/output.dir <- "results_low"/' | 
+                sed 's/dispersions <- .*/dispersions <- 0.00001/' > lowcounts.R
 Rscript lowcounts.R
                 
