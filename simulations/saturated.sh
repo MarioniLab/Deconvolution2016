70866f24e2374f29b9388fcbcbc7badfd134ea42
Aaron Lun <alun@wehi.edu.au>
2016-01-24 15:59:01 +0000

    Added very low count simulation results.

--- simulations/saturated.sh
+++ simulations/saturated.sh
@@ -1,3 +1,8 @@
 cat brittlesim.R | sed "s/true.means <- 2^runif(ngenes, 3, 6)/true.means <- 2^runif(ngenes, 9, 12)/" |
                 sed 's/output.dir <- "results"/output.dir <- "results_high"/' > highcounts.R
 Rscript highcounts.R
+
+cat brittlesim.R | sed "s/true.means <- 2^runif(ngenes, 3, 6)/true.means <- 2^runif(ngenes, 0, 1)/" |
+                sed 's/output.dir <- "results"/output.dir <- "results_low"/' > lowcounts.R
+Rscript lowcounts.R
+                
